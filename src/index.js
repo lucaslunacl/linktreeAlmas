01b689aa71ced4584b07ced975e9b6df845bcfd0
lucaslunaclaraso <lucaslunaclaraso@gmail.com>
2024-10-29 22:40:56 -0300

    init

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Registros from './paginas/Registros';
+import home from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Home Route */}
+        <Route path="/" element={<App />} />
+
+        {/* Akashic Records Page Route */}
+        <Route path="/registros" element={<Registros />} />
+
+        {/* Additional Routes can be added here */}
+        <Route path="/" element={<home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
